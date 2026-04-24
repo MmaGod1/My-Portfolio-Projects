bebedd6b083cf39fea79fd689c5e7d466423b4dd
MmaGod1 <gmmaduchukwu@gmail.com>
2026-04-24 13:21:20 +0100

    Add project 3 — used the Drag and Drop API

---
+++ 03-drag-and-drop/script.js
@@ -0,0 +1,49 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+// ----- DRAGGABLE ELEMENT FUNCTIONS ------
+function dragStart(ev) {
+  ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
+function dragEnd(ev) {
+  //
+}
+
+// ----- DROP ELEMENT FUNCTIO:NS ------
+function dragOver(ev) {
+  ev.preventDefault();
+}
+
+function dragEnter(ev) {
+  ev.preventDefault();
+  this.classList.add("over");
+}
+
+function dragLeave(ev) {
+  ev.preventDefault();
+  this.classList.remove("over");
+}
+
+function dragDrop(ev) {
+  ev.preventDefault();
+
+  const id = ev.dataTransfer.getData("text/plain");
+  const list = document.getElementById(id);
+
+  this.appendChild(list);
+  this.classList.remove("over");
+}
+
+// ----- ADD THE FUNCTIONS AND EVENT LISTENERS -----
+for (const list of lists) {
+  list.addEventListener("dragstart", dragStart);
+  list.addEventListener("dragend", dragEnd);
+}
+
+for (const card of cards) {
+  card.addEventListener("dragover", dragOver);
+  card.addEventListener("dragenter", dragEnter);
+  card.addEventListener("dragleave", dragLeave);
+  card.addEventListener("drop", dragDrop);
+}
